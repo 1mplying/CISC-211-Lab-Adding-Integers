71421b7aba24a263f418d260d34815c3033e49e8
1mplying <abacsbasc@gmail.com>
2026-09-08 22:11:53 -0700

    Update asmFunc.s

--- addition-starter-lab/firmware/src/asmFunc.s
+++ addition-starter-lab/firmware/src/asmFunc.s
@@ -9,7 +9,7 @@
 /* create a string */
     
 /*** STUDENTS: HINT: this is the string that the C code prints out!  **/
-nameStr: .asciz "Hello. My name is Inigo Montoya."  
+nameStr: .asciz "Hello. My name is Brennan Ryan."  
 
  .align
  
@@ -53,8 +53,7 @@ asmFunc:
     push {r4-r11,LR}
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-
-
+    ADD R0, R0, R1
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
     
     /* restore the caller's registers, as required by the 
